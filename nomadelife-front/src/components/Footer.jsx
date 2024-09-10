417bf328d6c28707a95acd41f57fa5151e4fbecf
Tawany2004 <tawanyvick@gmail.com>
2024-09-10 12:08:36 -0300

    Aula 0910

--- nomadelife-front/src/components/Footer.jsx
+++ nomadelife-front/src/components/Footer.jsx
@@ -1,7 +1,13 @@
-.footer{
-    height: 250px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    background-color: #edf3f6;
+import React from 'react'
+import styles from './Footer.module.css'
+
+export const Footer = () => {
+  return (
+    <>
+    <footer className={styles.footer}>
+        <h3>Plataforma para desenvolvedores que tem o espírito livre</h3>
+        <p>Nomade Developer</p>
+    </footer>
+    </>
+  )
 }
